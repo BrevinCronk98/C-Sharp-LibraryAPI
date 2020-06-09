c601083b0674e54512f0f165636185fc4865e85e
brevin <cronkbrevin98@gmail.com>
2020-06-09 13:33:14 -0700

    After lunch commit

--- Models/Genre.cs
+++ Models/Genre.cs
@@ -3,13 +3,18 @@ using System.ComonentModel.DataAnnotations;
 
 namespace LibraryAPI.Models
 {
-    public class GenreId
+    public class Genre
     {
         public int GenreId { get; set;}
         public string GenreName { get; set; }
         public string GenreDescription { get; set; }
+
+        public virtual ICollection<StoryGenre> Stories { get; set; }
         
-        
+        public Genre()
+        {
+            this.Stories = new HashSet<StoryGenre>()
+        }
 
     }
     
